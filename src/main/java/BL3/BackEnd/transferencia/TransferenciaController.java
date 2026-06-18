cc3a1eeddc418a1ef34d327adb50ab20d11f9ff3
Bruno  de Paula Fernandes <BrunoFernandes2315@outlook.com>
2026-06-18 16:25:15 -0300

    Documentção controller transferência

--- src/main/java/BL3/BackEnd/transferencia/TransferenciaController.java
+++ src/main/java/BL3/BackEnd/transferencia/TransferenciaController.java
@@ -2,11 +2,18 @@ package BL3.BackEnd.transferencia;
 
 import BL3.BackEnd.transferencia.dto.CreateTransferenciaDTO;
 import BL3.BackEnd.transferencia.dto.TransferenciaDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+@Tag(name = "Tranferências", description = "Operações de tranferências via pix.")
 
 @RestController
 @RequestMapping("/transferencia")
@@ -19,13 +26,24 @@ public class TransferenciaController {
         this.transferenciaService = transferenciaService;
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Retornar todas as transferências do usuário solicitado"),
+            @ApiResponse(responseCode = "400", description = "Usuário não existe, não encontrado e/ou não possuí transferências")
+    })
     @GetMapping("{userId}")
-    public ResponseEntity getAllByUser(@PathVariable int userId){
+    public ResponseEntity getAllByUser(
+        @Parameter(description = "Id do usuário.") @PathVariable int userId
+    ){
         List<TransferenciaDTO> response = transferenciaService.getAllByUser(userId);
         return ResponseEntity.ok(response);
     }
 
 
+    @Operation(summary = "Realiza uma transferência via pix",
+            description = "Debita do remetente e credita no destinatário")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Transferência realizada sem problemas.")
+    })
     @PostMapping
     public ResponseEntity createTranferencia(@RequestBody CreateTransferenciaDTO dto){
         transferenciaService.makeTransferencia(dto);
